39aadfc0cc3c0dc9a5f88f3c04eeb340c3377579
mstne03 <mserraitorne@gmail.com>
2025-06-09 17:00:32 +0200

    Setup trial deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/exit_web",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
